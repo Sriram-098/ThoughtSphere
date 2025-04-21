6beffd65a67790227cffaa78fca6be2d760bf202
Sriram-098 <sriramsenapathi842@gmail.com>
2025-04-21 19:58:06 +0530

    Allowed hosts in settings.py

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-qm0z*yt#+$4g(2x2z!05fhl$d96ju$t4r6s&n5r^ucs^%-d7ro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://thoughtsphere-xa4h.onrender.com','thoughtsphere-xa4h.onrender.com']
 
 
 # Application definition
